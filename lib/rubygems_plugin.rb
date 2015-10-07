3ca6d472a01062ffc8e0fcd97576729915baf812
Mengxiang Jiang <mj294@cornell.edu>
2015-10-07 14:45:47 -0600

    made message flashier

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -3,6 +3,15 @@ require 'rubygems/installer'
 Gem::Installer.class_eval do
   def say(message)
   	super unless message == spec.post_install_message
-  	puts "When you install gems, you must party hard!"
+  	message = <<-EOF
+\e[35;5m
+##################################################################
+#                                                                #
+# \e[0;1mWhen you Gem install, you must party hard!\e[0;5;35m #
+#                                                                #
+##################################################################
+\e[0m
+EOF
+  	puts message
   end
 end
